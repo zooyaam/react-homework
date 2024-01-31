dce31c57c555c35cac4d6938e396f184854182a4
zooyaam <yjoo2498@naver.com>
2024-01-31 15:02:17 +0900

    [화면 구성] 컴포넌트 분리 및 렌더링

---
+++ mission-01/src/components/Main/MyHistory.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+export default function MyHistory() {
+  return (
+    <section className="border-t-[5px] border-gray-200">
+      <h2 className="sr-only">나의 활동 목록</h2>
+      <ul className="relative">
+        <li className="w-full h-12 hover:bg-gray-100 hover:rounded transition-all duration-300 border-b-[1px] border-gray-200">
+          <button
+            type="button"
+            aria-label="활동배지 보기"
+            className="h-full w-full flex justify-between items-center"
+          >
+            <span className="text-paragraph-md ml-3">활동배지 10개</span>
+            <span className="bg-right-icon block bg-no-repeat bg-right w-4 h-4 mr-4" />
+          </button>
+        </li>
+        <li className="w-full h-12 hover:bg-gray-100 hover:rounded transition-all duration-300 border-b-[1px] border-gray-200">
+          <button
+            type="button"
+            aria-label="판매상품 보기"
+            id="sellingProduct"
+            className="h-full w-full flex justify-between items-center"
+          >
+            <span className="text-paragraph-md ml-3">판매상품 12개</span>
+            <span className="bg-right-icon block bg-no-repeat bg-right w-4 h-4 mr-4" />
+          </button>
+        </li>
+        <li className="w-full h-12 hover:bg-gray-100 hover:rounded transition-all duration-300">
+          <button
+            type="button"
+            aria-label="내가 받은 매너 평가 보기"
+            className="h-full w-full flex justify-between items-center"
+          >
+            <span className="text-paragraph-md ml-3">받은 매너 평가</span>
+            <span className="bg-right-icon block bg-no-repeat bg-right w-4 h-4 mr-4" />
+          </button>
+          <div
+            aria-label="내가 받은 매너 평가 목록"
+            className="py-[14px] px-3 absolute top-full left-0"
+          >
+            <div className="bg-people_black-icon bg-no-repeat flex gap-[9px] mb-3">
+              <span className="text-label-md ml-6">10</span>
+              <span className="text-paragraph-md bg-blue-100 p-2 rounded-[0_8px_8px_8px]">
+                시간 약속을 잘 지켜요.
+              </span>
+            </div>
+            <div className="bg-people_black-icon bg-no-repeat flex gap-[16px] mb-3">
+              <span className="text-label-md ml-6">9</span>
+              <span className="text-paragraph-md bg-blue-100 p-2 rounded-[0_8px_8px_8px]">
+                친절하고 매너가 좋아요
+              </span>
+            </div>
+            <div className="bg-people_black-icon bg-no-repeat flex gap-[16px]">
+              <span className="text-label-md ml-6">8</span>
+              <span className="text-paragraph-md bg-blue-100 p-2 rounded-[0_8px_8px_8px]">
+                제가 있는 곳까지 와서 거래했어요.
+              </span>
+            </div>
+          </div>
+        </li>
+        <li className="absolute w-full top-[315px] h-12 hover:bg-gray-100 hover:rounded transition-all duration-300 border-t-[1px] border-gray-200">
+          <button
+            type="button"
+            aria-label="내가 받은 거래 후기 보기"
+            className="h-full w-full flex justify-between items-center"
+          >
+            <span className="text-paragraph-md ml-3">받은 거래 후기</span>
+            <span className="bg-right-icon block bg-no-repeat bg-right w-4 h-4 mr-4" />
+          </button>
+          <div className="px-[10px] py-[14px]">
+            <div className="flex items-start gap-[9px]">
+              <img
+                src="/assets/profile-img.svg"
+                alt="윤이첵오의 프로필 이미지"
+                className="w-10 h-10 border rounded-full"
+              />
+              <div className="flex flex-col justify-center items-start">
+                <span aria-label="프로필 이름" className="text-label-sm">
+                  윤이첵오
+                </span>
+                <span
+                  aria-label="리뷰어 정보"
+                  className="text-paragraph-sm font-normal text-contents-content-secondary"
+                >
+                  구매자 ∙ 부산광역시 사하구 ∙ 4개월 전
+                </span>
+                <span className="text-paragraph-md font-normal">
+                  오메오메~! 감사합니당
+                </span>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+}
---
+++ mission-01/src/components/Main/Profile.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <section className="pt-[1.125rem] px-3">
+      <h2
+        id="profile"
+        aria-label="내 프로필"
+        className="flex justify-start items-center gap-[9px] mb-[18px]"
+      >
+        <img
+          src="/assets/profile-img.svg"
+          alt="내 프로필 사진"
+          id="userImg"
+          className="size-20 rounded-full shadow-[0_4px_4px_0_rgba(0,0,0,0.1)]"
+        />
+        <span className="text-label-lg">나는윤주</span>
+        <span
+          aria-label="내 검색 코드"
+          className="text-label-sm text-contents-content-secondary"
+        >
+          #011128
+        </span>
+      </h2>
+
+      <a
+        href=""
+        role="button"
+        aria-label="프로필 수정하기"
+        className="hover:brightness-95 block w-full px-7 py-3 my-[0.625rem] text-center text-label-md rounded bg-gray-100 text-bluegray-800"
+      >
+        프로필 수정
+      </a>
+      <div className="py-3">
+        <h2 className="text-xs font-normal bg-information-icon bg-no-repeat bg-[42px_1px]">
+          열정온도
+        </h2>
+        <img src="/assets/temperature.jpeg" alt="나의 열정 온도" />
+      </div>
+      <div className="py-4 flex justify-between gap-3">
+        <div aria-label="내가 받은 좋아요" className="basis-1/2">
+          <div className="flex gap-[5px]">
+            <div className="bg-empty_heart-icon block bg-no-repeat w-5 h-5 bg-center" />
+            <div className="flex items-center gap-[5px]">
+              <span className="text-paragraph-sm font-normal">받은 좋아요</span>
+              <span className="text-label-sm font-semibold">100%</span>
+            </div>
+          </div>
+          <span className="pl-[25px] block text-paragraph-sm font-normal text-contents-content-secondary">
+            11개 중 11명이 만족
+          </span>
+        </div>
+        <div aria-label="내 응답률" className="basis-1/2">
+          <div className="flex gap-[5px]">
+            <div className="bg-speech-icon block bg-no-repeat w-5 h-5 bg-center" />
+            <div className="flex items-center gap-[5px]">
+              <span className="text-paragraph-sm font-normal">응답률</span>
+              <span className="text-label-sm font-semibold">-%</span>
+            </div>
+          </div>
+          <span className="pl-[25px] block text-paragraph-sm font-normal text-contents-content-secondary">
+            표시될 만큼 충분히 채팅하지 않았어요.
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
