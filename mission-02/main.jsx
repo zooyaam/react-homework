d9e26e458e10ac5326376e3f736d9a73be431f9f
zooyaam <yjoo2498@naver.com>
2024-02-05 15:02:56 +0900

    [폴더 정리]

--- mission-02/main.jsx
+++ mission-02/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./src/App";
 import "./index.css";
 
 const rootElement = createRoot(document.querySelector("#root"));
